cdaacb74b62668a9b1a126e15504b95d4edd06af
Tsai Cheng Han <tsaichenghan999@gmail.com>
2023-04-11 22:58:46 +0800

    reset to first commit

--- app/src/main/java/com/neil/gitpractice/MainActivity.kt
+++ app/src/main/java/com/neil/gitpractice/MainActivity.kt
@@ -11,6 +11,7 @@ import com.neil.gitpractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
